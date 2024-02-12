fd837693ca1eb76fc3633ef56e276f6062e15224
Cesar Baez <baezcesar329@gmail.com>
2024-02-12 17:10:10 -0400

    Configuración WebSocket en backend

---
+++ api/src/main/java/com/acap/api/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.acap.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.acap.api.service.NotificationsService;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer{
+
+  private final NotificationsService notificationsService;
+
+  public WebSocketConfig (NotificationsService notificationsService) {
+    this.notificationsService = notificationsService;
+  }
+
+  @Override
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    registry.addHandler(notificationsService, "/notifications");
+  }
+}
